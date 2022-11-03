66e760a456bed886fe19c9704e8981414a4c1776
Shamkafs <rm-ben@mail.ru>
2022-11-03 14:02:39 +0300

    add task1

--- HomeWork1/Task1/Program.cs
+++ HomeWork1/Task1/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите целое число A: ");
+int intA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число B: ");
+int intB = Convert.ToInt32(Console.ReadLine());
+if (intA == intB)
+{
+    Console.WriteLine($"Числа A ({intA}) и B ({intB}) равны.");
+}
+else if (intA > intB)
+{
+    Console.WriteLine($"Число A ({intA}) больше числа B ({intB})");
+}
+else
+{
+    Console.WriteLine($"Число A ({intA}) меньше числа B ({intB})");
+}
