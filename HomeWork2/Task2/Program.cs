5eebb0bb1fdedc355cd2e436046f503dca1973f6
Shamkafs <rm-ben@mail.ru>
2022-11-06 23:25:46 +0300

    add task2

---
+++ HomeWork2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+int remainsDivision (int number)
+{
+    return (number % 10);
+}
+
+int removeLastDigit (int number)
+{
+    return (number / 10);
+}
+
+int newNumber = removeLastDigit(removeLastDigit(number)) * 10 + remainsDivision(number);
+System.Console.WriteLine($"{number} -> {newNumber}");
